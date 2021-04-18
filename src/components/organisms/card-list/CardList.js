95fd571ce84e4ed5bffbb657b76ed5d2bc0fc262
Gabriellji <gabriellji@yahoo.com>
2021-04-18 11:52:13 +0300

    feat: card list component

--- src/components/organisms/card-list/CardList.js
+++ src/components/organisms/card-list/CardList.js
@@ -2,16 +2,16 @@ import * as React from 'react';
 import { ApiContext } from '../../../context/ApiProvider';
 import Card from '../../atoms/card/Card';
 
-const addScore = (score) => score + 20;
+const addScore = (score, number) => score + number;
 
 const CardList = () => {
-    const { state, setState, getRandomCountries } = React.useContext(ApiContext);
+    const { state, getRandomCountries } = React.useContext(ApiContext);
     const [score, setScore] = React.useState(0);
 
     const clickHandler = (e) => {
         const id = e.target.id;
         if (id === state.countries[0].name.split(',')[0]) {
-            const newScore = addScore(score);
+            const newScore = addScore(score, 20);
             setScore(newScore);
             getRandomCountries();
         } else {
@@ -36,6 +36,7 @@ const CardList = () => {
                         capital={country.capital}
                     />)
             }
+            {/* TODO: Move score to component */}
             <h2>{score}</h2>
         </div>
     )
